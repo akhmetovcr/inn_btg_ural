9e12b4bf022ad051898ef2f7d811604b94aeff21
numpyordie <vladimirfortran@gmail.com>
2020-10-31 14:32:23 +0500

    импорт графа тюмени с помощью osmnx

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from OSMParser import download_osm
-
 if __name__ == "__main__":
-
-    a = download_osm(
-        left=65.2066,
-        bottom=57.0346,
-        right=65.9653,
-        top=57.2662
-    )
+    import osmnx as ox
+    # прямоугольник описанный вокруг Тюмени
+    north, east, south, west = 57.2662, 65.9653, 57.0346, 65.2066
+    region_total_graph = ox.graph_from_bbox(north=north, east=east, south=south, west=west, network_type='drive')
+    ox.plot_graph(region_total_graph)
