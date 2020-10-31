4d761aaf35c35e2560172b272fefa8f61b030b4d
numpyordie <vladimirfortran@gmail.com>
2020-10-31 14:00:03 +0500

    парсер с тегами, координаты тюмени

---
+++ main.py
@@ -0,0 +1,10 @@
+from OSMParser import download_osm
+
+if __name__ == "__main__":
+
+    a = download_osm(
+        left=65.2066,
+        bottom=57.0346,
+        right=65.9653,
+        top=57.2662
+    )
