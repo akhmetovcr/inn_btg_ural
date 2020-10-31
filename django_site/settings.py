1833a695725c5fe47b95003d414417b1042c6e7c
Karimov Radmir <kar.radmir@gmail.com>
2020-10-31 22:23:42 +0500

    Залил готовый шаблон сайта

--- django_site/settings.py
+++ django_site/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'velo'
+    'app',
+    'authentication',
+    'core',
+    'velo',
 ]
 
 MIDDLEWARE = [
